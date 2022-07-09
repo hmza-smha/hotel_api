7c4147a9a7e9e0530b54913b151f8d784882a9af
HamZa Akram Samha <smha.hmza@gmail.com>
2022-07-09 17:20:57 +0300

    Added Tables

--- hotel_api/hotel_api/Data/ApplicationDbContext.cs
+++ hotel_api/hotel_api/Data/ApplicationDbContext.cs
@@ -1,11 +1,27 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using hotel_api.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace hotel_api.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Hotel> Hotels { get; set; }
+
+        public DbSet<Room> Rooms { get; set; }
+
+        public DbSet<Amenity> Amenities { get; set; }
+
+
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Room>()
+                .HasKey(x => new { x.HotelId, x.RoomNumber });
         }
     }
 }
---
+++ hotel_api/hotel_api/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace hotel_api.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
